e7634ec08b4e6452b80ef057fcceae788824b412
Juan David Castrillon <jdcastri@bancolombia.com.co>
2024-05-25 11:55:33 -0500

    fix name usecase class

--- src/main/java/com/ias/eventos/domain/usecase/EventUseCase.java
+++ src/main/java/com/ias/eventos/domain/usecase/EventUseCase.java
@@ -6,13 +6,13 @@ import com.ias.eventos.domain.model.Events.gateway.RetrieveEvents;
 
 import java.util.List;
 
-public class EventIUseCase {
+public class EventUseCase {
 
     private final CreateEvent createEvent;
 
     private final RetrieveEvents retrieveEvents;
 
-    public EventIUseCase(CreateEvent createEvent, RetrieveEvents retrieveEvents) {
+    public EventUseCase(CreateEvent createEvent, RetrieveEvents retrieveEvents) {
         this.createEvent = createEvent;
         this.retrieveEvents = retrieveEvents;
     }
